be86de4da886fc051a9a51717c9470291c2b70d7
mizchi <miz404@gmail.com>
2017-07-05 04:02:58 +0900

    Ues react-navigation

--- App.js
+++ App.js
@@ -1,29 +1,119 @@
+/* @flow */
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { ScrollView, Text, View } from 'react-native'
+import { StackNavigator, TabNavigator } from 'react-navigation'
+import Ionicons from 'react-native-vector-icons/Ionicons'
 import { Button } from 'react-native-elements'
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Button
-          raised
-          icon={{ name: 'home', size: 32 }}
-          buttonStyle={{ backgroundColor: 'red', borderRadius: 10 }}
-          textStyle={{ textAlign: 'center' }}
-          title={`Welcome to\nReact Native Elements`}
-        />
-      </View>
-    )
+type ScreenProps = {
+  navigation: {
+    state: {
+      params: any
+    },
+    navigate: (string, ?Object) => void,
+    goBack: any => void
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
+const Nav = ({ navigation, banner }) =>
+  <ScrollView>
+    <Text style={{ padding: 10 }}>{banner}</Text>
+    <Button
+      onPress={() => navigation.navigate('Profile', { name: 'Jordan' })}
+      title="Open profile screen"
+    />
+    <Button
+      onPress={() => navigation.navigate('NotifSettings')}
+      title="Open notifications screen"
+    />
+    <Button
+      onPress={() => navigation.navigate('SettingsTab')}
+      title="Go to settings tab"
+    />
+    <Button onPress={() => navigation.goBack(null)} title="Go back" />
+  </ScrollView>
+
+const Home = ({ navigation }: ScreenProps) =>
+  <Nav banner="Home Screen" navigation={navigation} />
+
+const Profile = ({ navigation }: ScreenProps) =>
+  <Nav
+    banner={`${navigation.state.params.name}s Profile`}
+    navigation={navigation}
+  />
+
+const NotificationSettings = ({ navigation }: ScreenProps) =>
+  <Nav banner="Notifications Screen" navigation={navigation} />
+
+const Settings = ({ navigation }: ScreenProps) => <Text>Settings</Text>
+
+const MainTab = StackNavigator({
+  Home: {
+    screen: Home,
+    path: '/',
+    navigationOptions: {
+      title: 'Home'
+    }
+  },
+  Profile: {
+    screen: Profile,
+    path: '/people/:name',
+    navigationOptions: ({ navigation }) => ({
+      title: `${navigation.state.params.name}'s Profile!`
+    })
   }
 })
+
+const SettingsTab = StackNavigator({
+  Settings: {
+    screen: Settings,
+    path: '/',
+    navigationOptions: () => ({
+      title: 'Settings'
+    })
+  },
+  NotifSettings: {
+    screen: NotificationSettings,
+    navigationOptions: {
+      title: 'Notifications'
+    }
+  }
+})
+
+const App = TabNavigator(
+  {
+    Main: {
+      screen: MainTab,
+      path: '/',
+      navigationOptions: {
+        tabBarLabel: 'Home',
+        tabBarIcon: ({ tintColor, focused }) =>
+          <Ionicons
+            name={focused ? 'ios-home' : 'ios-home-outline'}
+            size={26}
+            style={{ color: tintColor }}
+          />
+      }
+    },
+    Settings: {
+      screen: SettingsTab,
+      path: '/settings',
+      navigationOptions: {
+        tabBarLabel: 'Settings',
+        tabBarIcon: ({ tintColor, focused }) =>
+          <Ionicons
+            name={focused ? 'ios-settings' : 'ios-settings-outline'}
+            size={26}
+            style={{ color: tintColor }}
+          />
+      }
+    }
+  },
+  {
+    tabBarPosition: 'bottom',
+    animationEnabled: false,
+    swipeEnabled: false
+  }
+)
+
+export default App
---
+++ flow-typed/libs.js
@@ -0,0 +1,10 @@
+/* @flow */
+declare module 'react-navigation' {
+  declare module.exports: any
+}
+declare module 'react-native' {
+  declare module.exports: any
+}
+declare module 'expo' {
+  declare module.exports: any
+}
