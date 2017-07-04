a70054aa3ca48e6a29b97da672e0b99835be2ed5
mizchi <miz404@gmail.com>
2017-07-05 01:38:35 +0900

    Add react-native-elements

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import { Button } from 'react-native-elements'
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
+        <Button
+          raised
+          icon={{ name: 'home', size: 32 }}
+          buttonStyle={{ backgroundColor: 'red', borderRadius: 10 }}
+          textStyle={{ textAlign: 'center' }}
+          title={`Welcome to\nReact Native Elements`}
+        />
       </View>
     )
   }
